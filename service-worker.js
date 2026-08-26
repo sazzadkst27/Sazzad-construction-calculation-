01502b534cb9fccdfad80c60a381d50faf1b8f42
Sazzad Hossain <shpeasskst@gmail.com>
2026-08-26 16:35:12 +0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="sazzad-construction-v17";
+const CACHE_NAME="sazzad-construction-v18";
 const CORE=["./","./index.html","./style.css","./script.js","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
